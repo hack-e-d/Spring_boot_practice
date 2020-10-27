0db57c4df519b6cf89c50f2c993844412f5aa49c
Vijay P V <vijayraj1998@yahoo.com>
2020-10-27 12:08:16 +0530

    Update 27/10/20

--- src/main/java/com/jarvis/demo1/HomeC.java
+++ src/main/java/com/jarvis/demo1/HomeC.java
@@ -1,18 +1,47 @@
 package com.jarvis.demo1;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @RestController
+@CrossOrigin
 public class HomeC {
 
+    private String uid;
+
     @RequestMapping("/")
-    public void home(){
+    public String home(){
 
         System.out.println("HI");
+        return "hi";
     }
 
+    @GetMapping("/validate")
+    public String validate( String uid,String pwd){
+        String id ="admin";
+        String password="1234";
+        if(id==uid&&password==pwd){
+            return ":)";
+        }
+        else {
+            return ":(";
+        }
+    }
+
+    @GetMapping("/time")
+    public LocalTime time(){
+        LocalTime myObj = LocalTime.now(); // Create a date object
+        System.out.println(myObj);
+        return myObj;
+    }
+
+    @GetMapping("/date")
+    public LocalDate date(){
+        LocalDate myObj = LocalDate.now(); // Create a date object
+        System.out.println(myObj);
+        return myObj;
+    }
 }
